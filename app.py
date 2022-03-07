7eb5d45529252c64c27562d5b54bec66fa3393b7
roko123477 <78839525+roko123477@users.noreply.github.com>
2022-03-07 20:19:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify,render_template
 import util
-from flask_cors import CORS
-app = Flask(__name__) CORS(app)
+
+app = Flask(__name__) 
 
 
 @app.route("/",methods=['GET'])
